0abcd4ad47a7d9a16bcd21fcfb57390d50730b61
7hoenix <jpholzmann@gmail.com>
2020-07-10 13:32:11 -0500

    Clickable squares

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,33 @@
 module Main exposing (Msg(..), main, update, view)
 
+import Array exposing (Array)
 import Browser
 import Html exposing (Html, button, div, span, text)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 
 
+type Color
+    = White
+    | Blue
+    | Black
+
+
+nextColor : Color -> Color
+nextColor input =
+    case input of
+        White ->
+            Blue
+
+        Blue ->
+            Black
+
+        Black ->
+            White
+
+
 type World
-    = World (List (List Int))
+    = World (Array (Array Color))
 
 
 type alias Model =
@@ -16,7 +37,15 @@ type alias Model =
 
 init : Model
 init =
-    Model (World [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ])
+    Model
+        (World
+            (Array.fromList
+                [ Array.fromList [ White, Blue, Black ]
+                , Array.fromList [ Blue, Black, Blue ]
+                , Array.fromList [ Blue, White, Blue ]
+                ]
+            )
+        )
 
 
 main =
@@ -24,14 +53,31 @@ main =
 
 
 type Msg
-    = NoOp
+    = ChangeColor Int Int -- X and Y coordinates in the grid.
 
 
 update : Msg -> Model -> Model
 update msg model =
+    let
+        (World world) =
+            model.world
+    in
     case msg of
-        NoOp ->
-            model
+        ChangeColor xCoordinate yCoordinate ->
+            case Array.get yCoordinate world of
+                Nothing ->
+                    model
+
+                Just row ->
+                    case Array.get xCoordinate row of
+                        Nothing ->
+                            model
+
+                        Just color ->
+                            { model
+                                | world =
+                                    World (Array.set yCoordinate (Array.set xCoordinate (nextColor color) row) world)
+                            }
 
 
 view : Model -> Html Msg
@@ -40,16 +86,41 @@ view model =
         [ viewWorld model.world ]
 
 
-viewRow : List Int -> Html Msg
-viewRow row =
-    div [] (List.map viewCell row)
+viewRow : Int -> Array Color -> Html Msg
+viewRow yCoordinate row =
+    div [] (Array.toList (Array.indexedMap (viewCell yCoordinate) row))
 
 
 viewWorld : World -> Html Msg
 viewWorld (World entries) =
-    div [] (List.map viewRow entries)
+    div [] (Array.toList (Array.indexedMap viewRow entries))
 
 
-viewCell : Int -> Html Msg
-viewCell v =
-    span [] [ text (String.fromInt v) ]
+viewCell : Int -> Int -> Color -> Html Msg
+viewCell yCoordinate xCoordinate color =
+    let
+        cssColor =
+            case color of
+                White ->
+                    "white"
+
+                Blue ->
+                    "lightblue"
+
+                Black ->
+                    "black"
+    in
+    span
+        [ style "display" "inline-block"
+        , style "background-color" cssColor
+        , style "width" cellSize
+        , style "border" "solid 1px lightgray"
+        , style "height" "3em"
+        , onClick (ChangeColor xCoordinate yCoordinate)
+        ]
+        []
+
+
+cellSize : String
+cellSize =
+    "2em"
